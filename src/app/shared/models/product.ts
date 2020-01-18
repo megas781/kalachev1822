b0cdc0f179ad1ba36ab0f62439ca2fa1f5a0147d
glebkalachev <megas781@gmail.com>
2020-01-18 10:56:44 +0300

    added to models initializers from json

--- src/app/shared/models/product.ts
+++ src/app/shared/models/product.ts
@@ -3,6 +3,17 @@ export abstract class Product {
   name: string;
   vendorCode: string;
   price: number;
+
+  protected constructor(id: number, name: string, vendorCode: string, price: number) {
+    this.id = id;
+    this.name = name;
+    this.vendorCode = vendorCode;
+    this.price = price;
+  }
+
+  public getPriceString() {
+    return `${this.price} руб`;
+  }
   public getDescription(): string {
     return `${this.vendorCode}, цена: ${this.price}`;
   }
@@ -10,6 +21,17 @@ export abstract class Product {
 export class Motherboard extends Product {
   chipset: string;
   formFactor: string;
+
+  constructor(id: number, name: string, vendorCode: string, price: number, chipset: string, formFactor: string) {
+    super(id, name, vendorCode, price);
+    this.chipset = chipset;
+    this.formFactor = formFactor;
+  }
+
+  public static init(id: string, name: string, vendorCode: string, price: string, chipset: string, formFactor: string) {
+    return new Motherboard(Number(id), name, vendorCode, Number(price), chipset, formFactor)
+  }
+
   public getDescription(): string {
     return super.getDescription() + `, чипсет: ${this.chipset}, форм-фактор: ${this.formFactor}`;
   }
@@ -18,12 +40,33 @@ export class CPU extends Product {
   socket: string;
   coreNumber: number;
   frequency: number;
+
+  constructor(id: number, name: string, vendorCode: string, price: number, socket: string, coreNumber: number, frequency: number) {
+    super(id, name, vendorCode, price);
+    this.socket = socket;
+    this.coreNumber = coreNumber;
+    this.frequency = frequency;
+  }
+
+  public static init(id: string, name: string, vendorCode: string, price: string, socket: string, coreNumber: string, frequency: string) {
+    return new CPU(Number(id), name, vendorCode, Number(price), socket, Number(coreNumber), Number(frequency))
+  }
+
   public getDescription(): string {
     return super.getDescription() + `, сокет: ${this.socket}, количество ядер: ${this.coreNumber}, частота: ${this.frequency}`;
   }
 }
 export class VideoCard extends Product {
   videoMemory: number;
+
+  constructor(id: number, name: string, vendorCode: string, price: number, videoMemory: number) {
+    super(id, name, vendorCode, price);
+    this.videoMemory = videoMemory;
+  }
+
+  public static init(id: string, name: string, vendorCode: string, price: string, videoMemory: string) {
+    return new VideoCard(Number(id), name, vendorCode, Number(price), Number(videoMemory))
+  }
   getDescription(): string {
     return super.getDescription() + `, видеопамять: ${this.videoMemory}`;
   }
