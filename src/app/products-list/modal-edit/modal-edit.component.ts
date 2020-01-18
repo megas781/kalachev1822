1fcb054464a28775327492d5d186aa3c112d0599
glebkalachev <megas781@gmail.com>
2020-01-18 15:49:44 +0300

    modal filler implemented

--- src/app/products-list/modal-edit/modal-edit.component.ts
+++ src/app/products-list/modal-edit/modal-edit.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Product} from '../../shared/models/product';
+import {CPU, Motherboard, Product, VideoCard} from '../../shared/models/product';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-modal-edit',
@@ -8,12 +9,55 @@ import {Product} from '../../shared/models/product';
 })
 export class ModalEditComponent implements OnInit {
 
-  // @Input('displayModal') displayModal: boolean;
   @Input('product') product?: Product;
+
+  formGroup: FormGroup;
+  filler = {category:'',id:0,name:'',price:0,vendorCode:'', chipset:'',formFactor:'', socket:'', coreNumber: 0, frequency: 0, videoMemory: 0};
 
   constructor() { }
 
   ngOnInit() {
+
+    let self = this;
+    if (self.product !== null) {
+
+    }
+
+    this.formGroup = new FormGroup({
+      "name": new FormControl(self.filler.name, Validators.required),
+      "price": new FormControl(self.filler.price, Validators.required),
+      "vendor-code": new FormControl(self.filler.vendorCode, Validators.required),
+      "category": new FormControl(self.filler.category, Validators.required),
+      "chipset": new FormControl(self.filler.chipset, Validators.required),
+      "form-factor": new FormControl(self.filler.formFactor, Validators.required),
+      "socket": new FormControl(self.filler.socket, Validators.required),
+      "core-number": new FormControl(self.filler.coreNumber, Validators.required),
+      "frequency": new FormControl(self.filler.frequency, Validators.required),
+      "video-memory": new FormControl(self.filler.videoMemory, Validators.required),
+    })
+  }
+
+  private applyProductValues(product: Product) {
+    if (product instanceof Motherboard) {
+      this.filler.category = 'motherboards';
+    } else if(product instanceof CPU) {
+      this.filler.category = 'cpu';
+    } else if(product instanceof VideoCard) {
+      this.filler.category = 'video-cards';
+    } else {
+      //do nothing
+    }
+
+    this.filler.id = product.id;
+    this.filler.name = product.name;
+    this.filler.vendorCode = product.vendorCode;
+    this.filler.price = product.price;
+    this.filler.chipset = product['chipset'];
+    this.filler.formFactor = product['formFactor'];
+    this.filler.socket = product['socket'];
+    this.filler.coreNumber = product['coreNumber'];
+    this.filler.frequency = product['frequency'];
+    this.filler.videoMemory = product['videoMemory'];
   }
 
 }
